61b55842bde9a3ae5147204253f42ad3a328204f
Manh Nguyen <manh.nguyen@thirdsight.net>
2024-12-14 12:21:29 +0800

    Pioneer Console Boilerplate

---
+++ Models/AppSettings.cs
@@ -0,0 +1,10 @@
+namespace MyConsole.Models
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Sets header title in console window
+        /// </summary>
+        public string ConsoleTitle { get; set; }
+    }
+}
